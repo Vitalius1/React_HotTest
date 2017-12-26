0bad785a5aa0744478e6f4305c6af45ba5c1586c
VitalieBraga <vitaliebraga@gmail.com>
2017-12-25 23:04:39 -0800

    when using TAB key we can focus either the tile or the cross/close button and pressing ENTER key on tile gives us the detail page for specific sauce. Pressing DELETE key on cross/close button deletes the tile from list

--- src/components/GridComponent/TileComponent/CloseButton.js
+++ src/components/GridComponent/TileComponent/CloseButton.js
@@ -3,20 +3,41 @@ import React from 'react';
 class CloseButton extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.handleClickCaptured = this.handleClickCaptured.bind(this);
+        this.handleKeyDownCaptured = this.handleKeyDownCaptured.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
-    
-    handleClick(e) {
+
+    handleClickCaptured(e) {
         e.stopPropagation();
         this.props.onClickClose(this.props.id);
     }
     
+    handleKeyDownCaptured(e) {
+        e.stopPropagation();
+        if (e.key === 'Backspace') {
+            this.props.onClickClose(this.props.id)
+        }
+    }
+
+    handleBlur() {
+    }
+
     render() {
-        if(!this.props.isMouseInside){
+        if (!this.props.isMouseInside) {
             return null;
         }
         return (
-            <div onClickCapture={this.handleClick.bind(this)}>
-                <span tabIndex={0} className='close' onClick={this.handleCl}>&times;</span>
+            <div onClickCapture={this.handleClickCaptured}
+                onKeyDownCapture={this.handleKeyDownCaptured}>
+                <span
+                    onKeyDown={this.handleKeyDown}
+                    tabIndex={0}
+                    onBlur={this.handleBlur}
+                    className='close'
+                    onClick={this.handleClick}
+                >&times;</span>
             </div>
         );
     }
--- src/components/GridComponent/TileComponent/Tile.js
+++ src/components/GridComponent/TileComponent/Tile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import Image from './Image';
 import Title from './Title';
 import Subtitle from './Subtitle';
@@ -15,24 +16,41 @@ class Tile extends React.Component {
         this.mouseLeave = this.mouseLeave.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.handleFocus = this.handleFocus.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
-
+    
+    componentDidMount() {
+        ReactDOM.findDOMNode(this).addEventListener("keydown", this.handleKeyDown, false);
+    }
+    
+    componentWillUnmount() {
+        ReactDOM.findDOMNode(this).removeEventListener("keydown", this.handleKeyDown, false);
+    }
+    
+    handleKeyDown(event) {
+        if (event.key == 'Enter') {
+            this.props.onClick(this.props.sauce.id);
+        }
+    }
+    
     mouseEnter() { // Hover-in mouse handler to show the close button
         this.setState({ isMouseInside: true });
     }
-
+    
     mouseLeave() { // Hover-out mouse handler to hide the close button
         this.setState({ isMouseInside: false });
     }
-
+    
     handleClose(id) {
         this.props.onClickClose(id);
     }
-
+    
     handleFocus() {
-       
+        this.setState({
+            isMouseInside: true
+        })
     }
-
+    
     render() {
         return (
             <div
