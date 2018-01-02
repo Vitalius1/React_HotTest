7a6c01fceaa8b93086e39af535d2a931e0194d73
VitalieBraga <vitaliebraga@gmail.com>
2018-01-01 20:18:12 -0800

    introduced a new dependancy : valuelink. --> used to make a two way binding on the forms inputs with the components state

--- src/components/AppComponent/HotSauceAddFormComponent/HotSauceAddForm.js
+++ src/components/AppComponent/HotSauceAddFormComponent/HotSauceAddForm.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Link, { LinkedComponent } from 'valuelink';
 
-class HotSauceAddForm extends React.Component {
+class HotSauceAddForm extends LinkedComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            title: '',
+            subtitle: '',
+            description: '',
+            imageURL: ''
+        }
     }
+
+    onSubmit(e) {
+        console.log("submiting", e.target);
+    }
+
     render() {
         if (!this.props.showAddFormModal) {
             return null;
         }
+        const linked = this.linkAll();
         return (
             <div className="App-AddSauceFormModal">
                 <span
@@ -17,7 +30,26 @@ class HotSauceAddForm extends React.Component {
                     &times;
                 </span>
                 <div className="App-AddSauceFormContent">
-                    <h1>Form Add Sauce</h1>
+                    <h1>ADD SAUCE</h1>
+                    <form onSubmit={this.onSubmit}>
+                        <label>Title:
+                            <input type="text" { ...linked.title.props } />
+                        </label><br />
+
+                        <label>Subtitle:
+                            <input type="text" { ...linked.subtitle.props } />
+                        </label><br />
+
+                        <label>Description:
+                            <input type="text" { ...linked.description.props } />
+                        </label><br />
+
+                        <label>Image URL:
+                            <input type="text" { ...linked.imageURL.props } />
+                        </label><br />
+
+                        <button type="submit">Add Sauce</button>
+                    </form>
                 </div>
             </div>
         )
